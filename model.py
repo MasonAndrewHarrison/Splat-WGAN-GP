715b8d3762addb71b759ef7e69492665bbff1079
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-02-02 18:23:53 -0500

    model

--- model.py
+++ model.py
@@ -5,19 +5,67 @@ import render
 
 #TODO create a PointNet (Point-wise MLPs) Model
 #TODO PointNet with knn with pointnet2_ops for knn gpu support
-#TODO look into using transformers and PointNet maybe?
+#TODO look into using transformers or EdgeConv?
 
 class Generator(nn.Module):
 
     def  __init__(self, latent_dim, features, num_points, out_dim):
         super(Generator, self).__init__()
 
+        self.num_points = num_points
+        self.features = features
+
+        self.linear_layers = nn.Sequential(
+            self._Linear_Block(latent_dim, features*4),
+            self._Linear_Block(features*4, features*16, use_layer_norm=False),
+            self._Linear_Block(features*16, features*32),
+            self._Linear_Block(features*32, features*64, use_layer_norm=False),
+            self._Linear_Block(features*64, num_points * features)
+        ) 
+
+        self.point_wise_mpls = nn.Sequential(
+            self._Point_Wise_MLP(features, features*2),
+            self._Point_Wise_MLP(features*2, features*4),
+            self._Point_Wise_MLP(features*4, features*8),
+        )
+
+        self.final_layer = nn.Conv1d(features*8, out_dim, 1)
+
+    @staticmethod
+    def _Linear_Block(in_channels, out_channels, use_layer_norm=True):
+
+        layers = [
+            nn.Linear(in_channels, out_channels)
+        ]
+        if use_layer_norm:
+            layers.append(nn.LayerNorm(out_channels))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+        layers.append(nn.Dropout(p=0.10))
+        return nn.Sequential(*layers)
+
+    # Also called shared MLP
+    @staticmethod
+    def _Point_Wise_MLP(in_channels, out_channels, use_instance_norm=True):
+        
+        layers = [
+            nn.Conv1d(in_channels, out_channels, 1)
+        ]
+        if use_instance_norm:
+            layers.append(nn.InstanceNorm1d(out_channels, affine=True)) 
+        layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers)
+
     def forward(self, x):
-        return x
+
+        out = self.linear_layers(x)
+        out = out.reshape(-1, self.features, self.num_points)
+        out = self.point_wise_mpls(out)
+        out = self.final_layer(out)
+        return out
 
 class PC_Critic(nn.Module):
 
-    def  __init__(self, features, num_points, in_dim):
+    def  __init__(self, features, in_dim):
         super(PC_Critic, self).__init__()
 
         self.point_wise_mpls = nn.Sequential(
@@ -30,53 +78,33 @@ class PC_Critic(nn.Module):
             self._Linear_Block(features*32, features*16),
             self._Linear_Block(features*16, features*8),
             self._Linear_Block(features*8, features*4),
-            self._Linear_Block(features*4, 1)
-
+            nn.Linear(features*4, 1)
         )
 
     # Also called shared MLP
     @staticmethod
-    def _Point_Wise_MLP(in_channels, out_channels, use_batch_norm=True):
+    def _Point_Wise_MLP(in_channels, out_channels):
         
-        layers = [
-            nn.Conv1d(in_channels, out_channels, 1)
-        ]
-        if use_batch_norm:
-            layers.append(nn.BatchNorm1d(out_channels)) 
-        layers.append(nn.ReLU(inplace=True))
-        return nn.Sequential(*layers)
+        return nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, 1),
+            nn.ReLU(inplace=True)
+        )
 
-    def _Linear_Block(in_channels, out_channels, use_batch_norm=True):
+    @staticmethod
+    def _Linear_Block(in_channels, out_channels):
 
-        layers = [
-            nn.Linear(in_channels, out_channels)
-        ]
-        if use_batch_norm:
-            layers.append(nn.BatchNorm1d(out_channels))
-        layers.append(nn.ReLU(inplace=True))
-        layers.append(nn.Dropout(p=0.25))
-        return nn.Sequential(*layers)
+        return nn.Sequential(
+            nn.Linear(in_channels, out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
         out = self.point_wise_mpls(x)
         out_max = torch.max(out, 2)[0]
         out_avg = torch.mean(out, 2)
         out = torch.cat([out_max, out_avg], dim=1)
-        print(out.shape)
         out = self.linear_layers(out)
         return out
-
-
-if __name__ == '__main__':
-    
-    point_cloud = torch.randn(1, 6, 1024)
-    print(point_cloud.shape)
-    critic = PC_Critic(32, 1024, 6)
-    out = critic(point_cloud)
-    print(out.shape)
-
-
-
 
 def initialize_weight(model):
 
@@ -88,3 +116,15 @@ def initialize_weight(model):
         elif isinstance(m, nn.BatchNorm1d):
             nn.init.constant_(m.weight.data, 1.0)
             nn.init.constant_(m.bias.data, 0.0)
+
+if __name__ == '__main__':
+    
+    latent = torch.randn(1, 128)
+    generator = Generator(128, features=32, num_points=1024, out_dim=6)
+    point_cloud = generator(latent)
+    print(point_cloud.shape)
+    critic = PC_Critic(features=32, in_dim=6)
+    out = critic(point_cloud)
+    print(out.shape)
+
+
